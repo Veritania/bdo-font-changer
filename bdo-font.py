f3fbea9dad180609bbd200c477f79e3ced08e156
Veritania <jesper_ant@hotmail.com>
2022-01-12 12:57:16 +0100

    working font selection dropdown

--- bdo-font.py
+++ bdo-font.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter.ttk import *  # Overrides tk default ui to instead match OS
 from tkinter import filedialog
 import tkinter as tk
+from tkinter import font
 
 
 def browseFiles():
@@ -29,6 +30,24 @@ game_dir_entry.place(x=40, y=30)
 
 browse_button = Button(root, text="Browse...", command=browseFiles)
 browse_button.place(x=370, y=30)
+
+
+# Font Selection
+font_label = Label(root, text="Select custom font")
+font_label.place(x=40, y=60)
+
+fonts = list(font.families())
+
+# TODO: Filter fonts to only get .ttf (TrueType Fonts)
+font_collection = StringVar(root)
+font_collection.set(fonts)
+
+font_dropdown = OptionMenu(root, font_collection, *font.families())
+font_dropdown.place(x=40, y=80)
+
+for item in range(0, int(font_dropdown['menu'].index('end'))):
+    font_dropdown['menu'].entryconfig(
+        item, font=font.Font(family=font.families()[item]))
 
 
 # Patch Font Button
